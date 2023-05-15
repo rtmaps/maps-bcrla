7edeeadc2c97a45a5e49c4f535047fe7a8fc98f4
rtmaps <133635904+rtmaps@users.noreply.github.com>
2023-05-15 15:19:36 +0700

    init

---
+++ google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/11NjnjfEkNDObqhYQN5Xn4K40gQ6qoPU6PUMrSZ7Fcz4/';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
